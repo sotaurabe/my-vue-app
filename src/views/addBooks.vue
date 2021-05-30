fd81504e7779edccf02ac3afc317777e56f87557
sotaurabe <tacchoso@icloud.com>
2021-05-31 03:08:29 +0900

    参考書登録ページで参考書データをデータベースに登録できるようにした

--- src/views/addBooks.vue
+++ src/views/addBooks.vue
@@ -1,8 +1,75 @@
 <template>
   <div>
     <p>参考書の追加</p>
-    <input type="text" placeholder="参考書名" />
-    <input type="text" placeholder="出版社名" />
-    <button>追加</button>
+    <div id="selectSubject">
+      <label
+        ><input type="radio" value="fhy" v-model="selectSubject" />物理</label
+      >
+      <label
+        ><input type="radio" value="che" v-model="selectSubject" />化学</label
+      >
+      <label
+        ><input type="radio" value="bio" v-model="selectSubject" />生物</label
+      >
+    </div>
+    <input type="text" placeholder="参考書名" v-model="bookName" />
+    <input type="text" placeholder="出版社名" v-model="bookCompany" />
+    <button v-on:click="addBooks">追加</button>
   </div>
 </template>
+
+<script>
+import firebase from "firebase"
+export default {
+  data() {
+    return {
+      selectSubject: "",
+      bookName: "",
+      bookCompany: "",
+      phyBooks: [],
+      cheBooks: [],
+      bioBooks: [],
+    }
+  },
+  methods: {
+    addBooks() {
+      const book = {
+        Name: this.bookName,
+        Company: this.bookCompany,
+      }
+
+      if (this.selectSubject === "fhy") {
+        firebase
+          .firestore()
+          .collection("phyBooks")
+          .add(book)
+          .then(() => {
+            this.phyBooks.push(book)
+          })
+        this.bookName = ""
+        this.bookCompany = ""
+      } else if (this.selectSubject === "che") {
+        firebase
+          .firestore()
+          .collection("cheBooks")
+          .add(book)
+          .then(() => {
+            this.cheBooks.push(book)
+          })
+        this.bookName = ""
+        this.bookCompany = ""
+      } else {
+        firebase
+          .firestore()
+          .collection("bioBooks")
+          .add(book)
+          .then(() => {
+            this.phyBooks.push(book)
+          })
+        this.bookName = ""
+        this.bookCompany = ""
+      }
+    },
+  },
+}
+</script>
