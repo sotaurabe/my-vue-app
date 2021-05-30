2c23459279a9438bda0eafbee397d34bc0746fb2
sotaurabe <tacchoso@icloud.com>
2021-05-30 23:14:05 +0900

    firebaseの導入

---
+++ src/firebase.js
@@ -0,0 +1,14 @@
+import firebase from "firebase"
+import "firebase/firestore"
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDtxPsLMRt4ox_lqkc-h0hUC47E-Gzb2-A",
+  authDomain: "study-books-912fd.firebaseapp.com",
+  projectId: "study-books-912fd",
+  storageBucket: "study-books-912fd.appspot.com",
+  messagingSenderId: "810673178783",
+  appId: "1:810673178783:web:7f3db9d2902e728ed8faaa",
+  measurementId: "G-H8XYX0TJ4C",
+}
+
+firebase.initializeApp(firebaseConfig)
